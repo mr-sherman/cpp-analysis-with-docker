7c3b4a1331def1737e5058094a38311bac8a2217
mr-sherman <77112096+mr-sherman@users.noreply.github.com>
2021-07-22 14:56:22 -0400

    Update helloworld2.cpp

--- HelloWorld/helloworld2.cpp
+++ HelloWorld/helloworld2.cpp
@@ -6,8 +6,11 @@ using namespace std;
 int main(int argc, char **argv)
 {
   cout << "Hello world " << endl;
-  
+  const char *message = "Hello";
+  char password[32];
+  char buffer[256];
 
+  memcpy(buffer, message, 256);
  
   return 0;
 }
